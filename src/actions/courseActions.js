dc3f33c00c817e58206dbbb03b6f874d7c5b8114
Anh Le <anh.le@beteasy.com>
2018-12-28 11:11:54 +1100

    fix: Error indicator display issue.

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
-import { beginAjaxCall } from './ajaxStatusAction';
+import { beginAjaxCall, ajaxCallError } from './ajaxStatusAction';
 
 export function createCourse(course) {
   return {
@@ -48,6 +48,7 @@ export function saveCourse(course) {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
     }).catch(error => {
+      dispatch(ajaxCallError());
       throw(error);
     });
   };
